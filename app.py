c3846e35de82fb0fd05da67567e9be583c30b48c
NurdinDev <nourba691@gmail.com>
2020-06-07 22:45:20 +0300

    :rocket: keep learning

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, url_for, redirect, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nureddin@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default= False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+@app.route('/')
+def index():
+    data = Todo.query.all()
+    return render_template('index.html', data=data)
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info)
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
